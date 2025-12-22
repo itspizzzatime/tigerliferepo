3c0b28d4242bb5a063abab31f1e3f9aff7417ad7
MARRON FRANCIA <marron.francia.sci@ust.edu.ph>
2025-12-22 12:39:01 +0000

    Label is not defined
    src/app/login/page.tsx (49:18) @ Object.render

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
+  FormLabel as Label,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
